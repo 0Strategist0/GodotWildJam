382d03fd810d3877ca8be30e014f7cc160871129
Bryan <bryandang10@gmail.com>
2024-06-17 23:51:11 -0700

    Reduce code repetition from move func

--- GameScenes/Objects/Movables/door.gd
+++ GameScenes/Objects/Movables/door.gd
@@ -9,27 +9,20 @@ var activated := false
 var currently_moving := false
 var original_position: Vector2
 
-const DISTANCE_TO_MOVE := Vector2(0, -50)
-const TIME_TO_MOVE := 0.5
+const DISTANCE_UP := Vector2(0, -50)
+const DISTANCE_DOWN := Vector2(0, 50)
+const TIME_TO_MOVE := 1
 
 
 func _ready() -> void:
 	original_position = position
 
 
-func _move_up() -> void:
+func _move(direction: Vector2) -> void:
 	currently_moving = true
-	var target_position = original_position if activated else original_position + DISTANCE_TO_MOVE
+	var target_position = original_position if activated else original_position + direction
 	var tween = create_tween()
 	tween.tween_property(self, "position", target_position, TIME_TO_MOVE)
-	tween.connect("finished", on_tween_finished)
-
-
-func _move_down() -> void:
-	currently_moving = true
-	var target_position = original_position if activated else original_position + DISTANCE_TO_MOVE
-	var tween = create_tween()
-	tween.tween_property(self, "position", position + Vector2(0,50), TIME_TO_MOVE)
 	tween.connect("finished", on_tween_finished)
 
 
@@ -50,9 +43,9 @@ func _handle_signal() -> void:
 	
 	match door_behaviour:
 		BEHAVIOUR.MOVE_UP:
-			_move_up()
+			_move(DISTANCE_UP)
 		BEHAVIOUR.MOVE_DOWN:
-			_move_down()
+			_move(DISTANCE_DOWN)
 		BEHAVIOUR.DISAPPEAR:
 			_disappear()
 			
