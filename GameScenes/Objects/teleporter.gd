33b2b0398dd32012267263c084843f18ec039da2
Bryan <bryandang10@gmail.com>
2024-06-17 17:24:44 -0700

    Finish teleporter

--- GameScenes/Objects/teleporter.gd
+++ GameScenes/Objects/teleporter.gd
@@ -4,6 +4,7 @@ extends Area2D
 
 @onready var main := GlobalNodeReferences.main
 
+# Teleports the user to the set Marker2D
 func _on_body_entered(body: Node2D) -> void:
 	if body == GlobalNodeReferences.character:
 		body.position = teleport_location.global_position 
