9efe84f945cf8fcfdf2ffbae23f2f3c48ec7e884
0Strategist0 <0strategist0@gmail.com>
2024-06-20 01:04:48 -0700

    Hunter animation 1
    First part of hunter animation

--- GameScenes/Levels/Hunter.gd
+++ GameScenes/Levels/Hunter.gd
@@ -3,7 +3,7 @@ extends Area2D
 
 func _on_trigger_hunt_body_entered(body: Node2D) -> void:
 	if body == GlobalNodeReferences.character:
-		$AnimationPlayer.play("hunter1_animation")
+		$AnimationPlayer.play("Hunt")
 
 
 func _on_body_entered(body: Node2D) -> void:
