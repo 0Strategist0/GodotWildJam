e1bf9f22b4321c07a2af0dd2dc2dfc70b01a8e7d
Bryan <bryandang10@gmail.com>
2024-06-21 12:48:39 -0700

    Let platforms pause in midair

--- GameScenes/Objects/Movables/platform.gd
+++ GameScenes/Objects/Movables/platform.gd
@@ -2,7 +2,7 @@ extends AnimatableBody2D
 
 ## If there is no switch, the platform will move on its own.
 @export var uses_switch: bool = false
-## An array of markers that the platform will travel to. The platform's original position will be included in the rotation of points.
+## An array of markers that the platform will travel to. Must create a Marker for every spot (including current) 
 @export var points: Array[Marker2D] = []
 ## How fast the platform should move.
 @export var speed: int = 50
@@ -13,14 +13,12 @@ var objects_in_zone_counter: int = 0
 # Variables used for moving calculation
 var activated: bool = false
 var currently_moving: bool = false
-var next_point_idx: int = 0
+var next_point_idx: int = 1
+var tween: Tween
 
 
 func _ready() -> void:
-	var original_position := Marker2D.new()
-	original_position.position = position
-	points.insert(0, original_position)
-	movable = (points.size() != 0)
+	movable = (points.size() != 1)
 
 
 func _process(_delta: float) -> void:
@@ -38,10 +36,8 @@ func _move_to_next_point() -> void:
 	# This is needed to resolve a crash when creating tween when not in tree
 	if not is_inside_tree():
 		return
-
+	
 	# Find next position to move towards
-	next_point_idx += 1
-	next_point_idx %= points.size()
 	var target_position := points[next_point_idx].position
 	
 	# Calculate speed to keep movement consistent
@@ -49,12 +45,14 @@ func _move_to_next_point() -> void:
 	var tween_time := tween_distance / speed
 	
 	# Create tween to move to desired location
-	var tween: Tween = create_tween()
+	tween = create_tween()
 	tween.tween_property(self, "position", points[next_point_idx].position, tween_time)
 	tween.connect("finished", on_tween_finished)
 
 
 func on_tween_finished() -> void:
+	next_point_idx += 1
+	next_point_idx %= points.size()
 	currently_moving = false
 
 
@@ -68,7 +66,14 @@ func _on_plate_switch_body_exited(_body: Node2D) -> void:
 	objects_in_zone_counter -= 1
 	if objects_in_zone_counter == 0:
 		activated = false
+		if tween.is_valid():
+			currently_moving = false
+			tween.kill()
 
 
 func _on_lever_switch_lever_toggled(_number: int) -> void:
 	activated = !activated
+	if not activated:
+		if tween.is_valid():
+			currently_moving = false
+			tween.kill()
