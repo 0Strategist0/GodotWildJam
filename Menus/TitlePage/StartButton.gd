e44c1676c2d0d363e403ece4b73e325497bb34d2
Lucas Mah <lucasmah@outlook.com>
2024-06-21 22:49:33 -0700

    Revert "Merge branch 'main' into its-workin"
    This reverts commit 1dbe86cfd3f388dc40adc271f360dce5ffbf5d6e, reversing
    changes made to 4070fb8e257cfd99e3f02719fbc2584bf44ec775.

--- Menus/TitlePage/StartButton.gd
+++ Menus/TitlePage/StartButton.gd
@@ -3,21 +3,20 @@ extends Button
 const WORLD := preload("uid://cp08dx7in60dn")
 const LEVEL1 := preload("uid://b0kxtk2p027ml")
 const UI := preload("uid://b5kvf0loq0rxl")
-# for some reason this doesn't exist
-#const SIMON := preload("uid://c4c0m1vjycehs")
+const SIMON := preload("uid://c4c0m1vjycehs")
 const A1 := preload("uid://b757fjeorrns1")
 
 @onready var main := GlobalNodeReferences.main
 @onready var title_page := get_node("../..")
 
 # Remove this later
-var debug_mode := 2
+var debug_mode := 1
 
 func _on_pressed() -> void:
 	var world := WORLD.instantiate()
 	var level := LEVEL1.instantiate()
 	var ui := UI.instantiate()
-	#var simon := SIMON.instantiate()
+	var simon := SIMON.instantiate()
 	var a1 := A1.instantiate()
 	
 	match debug_mode:
@@ -26,8 +25,7 @@ func _on_pressed() -> void:
 		2:
 			main.add_child(level)
 		3:
-			#main.add_child(simon)
-			pass
+			main.add_child(simon)
 		4:
 			main.add_child(a1)
 	main.add_child(ui)
