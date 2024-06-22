8b30cc133e0c6100d2d5284b3a6dae6050761ff0
Bryan <bryandang10@gmail.com>
2024-06-21 20:14:09 -0700

    Add section to b1

--- Menus/TitlePage/StartButton.gd
+++ Menus/TitlePage/StartButton.gd
@@ -3,20 +3,21 @@ extends Button
 const WORLD := preload("uid://cp08dx7in60dn")
 const LEVEL1 := preload("uid://b0kxtk2p027ml")
 const UI := preload("uid://b5kvf0loq0rxl")
-const SIMON := preload("uid://c4c0m1vjycehs")
+# for some reason this doesn't exist
+#const SIMON := preload("uid://c4c0m1vjycehs")
 const A1 := preload("uid://b757fjeorrns1")
 
 @onready var main := GlobalNodeReferences.main
 @onready var title_page := get_node("../..")
 
 # Remove this later
-var debug_mode := 1
+var debug_mode := 2
 
 func _on_pressed() -> void:
 	var world := WORLD.instantiate()
 	var level := LEVEL1.instantiate()
 	var ui := UI.instantiate()
-	var simon := SIMON.instantiate()
+	#var simon := SIMON.instantiate()
 	var a1 := A1.instantiate()
 	
 	match debug_mode:
@@ -25,7 +26,8 @@ func _on_pressed() -> void:
 		2:
 			main.add_child(level)
 		3:
-			main.add_child(simon)
+			#main.add_child(simon)
+			pass
 		4:
 			main.add_child(a1)
 	main.add_child(ui)
